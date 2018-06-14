bab7bad9d0ecfcdcc0666137cddd043f135e4778
shunqi-wang <36658469+shunqi-wang@users.noreply.github.com>
2018-06-14 16:44:11 +0800

    Add config feature

--- app/src/main/java/com/example/chooh/myapplication/Main.java
+++ app/src/main/java/com/example/chooh/myapplication/Main.java
@@ -1,11 +1,15 @@
 package com.example.chooh.myapplication;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -21,6 +25,7 @@ public class Main extends AppCompatActivity implements View.OnClickListener{
     private ArrayList<Sensor> selected=new ArrayList<>();
     private ArrayList<String> sensorNames=new ArrayList<>();
     private ArrayList<Sensor> sensors;
+    private TelephonyManager telephonyManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +51,8 @@ public class Main extends AppCompatActivity implements View.OnClickListener{
                 recreate();
             }
         });
+
+        telephonyManager=(TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
     }
 
     @Override
@@ -54,6 +61,11 @@ public class Main extends AppCompatActivity implements View.OnClickListener{
         int length=0;
         for(boolean b:ba){
             if(b)length++;
+        }
+
+        if(checkSelfPermission(Manifest.permission.READ_PHONE_STATE)!=PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this,"Please enable the PHONE permission!",Toast.LENGTH_LONG).show();
+            return;
         }
 
         if(length==0){
@@ -68,70 +80,6 @@ public class Main extends AppCompatActivity implements View.OnClickListener{
                 ia[pointer++]=i;
             }
         }
-
-//        for(int i:ia){
-//            Sensor sensor=sensors.get(i);
-//            selected.add(sensor);
-//            sensorNames.add(sensorAdapter.sensorTypeToString(sensor.getType()));
-//        }
-//
-//        JSONObject names=null;
-//        try{
-//            AssetManager assetManager=getAssets();
-//            InputStream is=assetManager.open("Test.json");
-//            String content="";
-//            InputStreamReader ir=new InputStreamReader(is);
-//            BufferedReader br=new BufferedReader(ir);
-//            String st;
-//            while ((st=br.readLine())!=null){
-//                content+=st;
-//            }
-//            System.out.println(content);
-//            names=new JSONObject(content);
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//
-//        final JSONObject object=new JSONObject();
-//        final JSONObject final_names=names;
-//
-//        for(int j=0;j<ia.length;j++){
-//            final int i=j;
-//            final int len=ia.length-1;
-//            SensorEventListener listener=new SensorEventListener() {
-//                @Override
-//                public void onSensorChanged(SensorEvent event) {
-//                    if(event.sensor.equals(selected.get(i))){
-//                        JSONArray nameArray=null;
-//                        try{
-//                            nameArray=(JSONArray)final_names.get(sensorNames.get(i));
-//                        }catch(JSONException e){
-//                            e.printStackTrace();
-//                        }
-//                        try{
-//                            for(int x=0;x<event.values.length;x++){
-//                                if(nameArray==null){
-//                                    object.put(sensorNames.get(i)+"-unknown-key"+(x+1),event.values[x]);
-//                                }else{
-//                                    object.put(sensorNames.get(i)+"-"+(String)nameArray.get(x),event.values[x]);
-//                                }
-//                            }
-//
-//                            Log.i("shunqi",object.toString());
-//                        }catch (JSONException e){
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }
-//
-//                @Override
-//                public void onAccuracyChanged(Sensor sensor, int accuracy) {}
-//            };
-//
-//            manager.registerListener(listener,selected.get(i),3333333);
-//        }
-
 
         Intent intent=new Intent(this, Info.class);
         intent.putExtra("select",ia);
